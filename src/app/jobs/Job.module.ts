2fc630e9cb7a71590442aa1f2a2536102c67af9b
Dereje Kitaw <dkitaw@solareworld.local>
2017-05-08 14:29:31 -0400

    job-detail component added

--- src/app/jobs/Job.module.ts
+++ src/app/jobs/Job.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { JobListComponent } from './job-list/job-list.component';
+import { JobDetailComponent } from './job-detail/job-detail.component';
+
 
 import { JobService } from './job.service';
+
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
-      { path: 'jobs', component: JobListComponent }  ])
+      { path: 'jobs', component: JobListComponent },
+      { path: 'job/:id',
+
+        component: JobDetailComponent
+      }  ])
   ],
     providers: [JobService],
-  declarations: [JobListComponent]
+  declarations: [JobListComponent, JobDetailComponent]
 })
 export class JobModule { }
---
+++ src/app/jobs/job-detail/job-detail.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Subscription }       from 'rxjs/Subscription';
+
+import { IJob } from '../job';
+import { JobService } from '../job.service';
+@Component({
+  selector: 'sew-job-detail',
+  templateUrl: './job-detail.component.html',
+  styleUrls: ['./job-detail.component.css']
+})
+export class JobDetailComponent implements OnInit {
+pageTitle: string = 'Job Detail';
+    job: IJob;
+    errorMessage: string;
+    private sub: Subscription;
+
+    constructor(private _route: ActivatedRoute,
+                private _router: Router,
+                private _jobService: JobService) {
+    }
+  
+
+  ngOnInit() {
+    this.sub = this._route.params.subscribe(
+            params => {
+                let id = +params['id'];
+                this.getJob(id);
+        });
+  }
+getJob(id: number) {
+        this._jobService.getJob(id).subscribe(
+            job => this.job = job,
+            error => this.errorMessage = <any>error);
+    }
+}
