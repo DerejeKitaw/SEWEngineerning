dfc7ca5e29b520c259c1ca5e5ba9dfc3f4bc753d
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-05-07 23:45:23 -0400

    routin added in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
 
 /** Feature Modules */
-import {JobModule} from './jobs/Job.module'
+import {JobModule} from './jobs/Job.module';
+import { HomeComponent } from './Home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent
     
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: HomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', component: PageNotFoundComponent }
+    ]),
     JobModule
   ],
   providers: [],
