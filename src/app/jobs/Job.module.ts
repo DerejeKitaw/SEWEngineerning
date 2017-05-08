527858aa9064d9ee15ce998dcca09d9533309b6a
Dereje Kitaw <dereje.kitaw@gmail.com>
2017-05-07 23:45:58 -0400

    routModule added  for job module

--- src/app/jobs/Job.module.ts
+++ src/app/jobs/Job.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+//import { CommonModule } from '@angular/common';
 import { JobListComponent } from './job-list/job-list.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    //CommonModule,
+    RouterModule.forChild([
+      { path: 'jobs', component: JobListComponent }  ])
   ],
   declarations: [JobListComponent]
 })
