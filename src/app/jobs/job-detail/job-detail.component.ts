6b91544acbf1e5c9a7a8e446553d306884d65ff6
Dereje Kitaw <dkitaw@solareworld.local>
2017-05-08 16:07:45 -0400

    unsubscribe the promiss if jobdetail closed

--- src/app/jobs/job-detail/job-detail.component.ts
+++ src/app/jobs/job-detail/job-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Router, ActivatedRoute } from '@angular/router';
 
@@ -33,6 +33,9 @@ pageTitle: string = 'Job Detail';
 
         });
   }
+   ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
 getJob(id: number) {
         this._jobService.getJob(id).subscribe(
             job => this.job = job,
